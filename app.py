2afd2053b3f4eb28aa69c9ca790be8af6445966c
Modou Niang <modouniang@modous-mbp.attlocal.net>
2020-10-08 12:11:45 -0500

    student and admin button routing complete

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+@app.route('/student')
+def student():
+	return render_template('student.html')
+
+@app.route('/admin')
+def admin_home():
+	return render_template('admin.html')
+'''
+@app.route('/adminLoginAuth', methods=['POST'])
+def login_auth():
+	pass
+
+@app.route('/adminLogin', methods=['GET'])
+def login_auth():
+	pass
+
+
+@app.route('/postGroup', methods=['POST'])
+def post_group():
+	pass
+'''
 
 if __name__ == "__main__":
 	app.run(debug=True)
