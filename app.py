c50bf4ef0af7b01058410452549d011bc41e6d5b
Modou Niang <modouniang@modous-mbp.attlocal.net>
2020-10-08 14:26:32 -0500

    ready for database connection

--- app.py
+++ app.py
@@ -8,11 +8,40 @@ def home():
 
 @app.route('/student')
 def student():
-	return render_template('student.html')
+	#Get available topics from # DB and pass it to the frontend
+	topics = ['Mixed Reality & the Metaverse',
+			  'Quantum Computing',
+			  'Nanotechnology or Nanobiotechnology',
+			  'Bioprinting',
+			  'Brain-machine interface',
+			  'IoT & Cloud Computing',
+			  'Autonomous Vehicles',
+			  'Commercial UAV',
+			  'Digital Fabrication',
+			  'Blockchain']
+
+	return render_template('student.html', topics=topics)
 
 @app.route('/admin')
 def admin_home():
 	return render_template('admin.html')
+
+@app.route('/postGroup', methods=['GET', 'POST'])
+def post_group():
+	if request.form:
+		data = request.form
+		#Get data from frontend
+		name = data['name']
+		netID = data['netId']
+
+		topic = data['topic']
+
+
+		#Check if netID has already been registered
+
+		#Add to the database, if success
+	pass
+
 '''
 @app.route('/adminLoginAuth', methods=['POST'])
 def login_auth():
@@ -22,11 +51,18 @@ def login_auth():
 def login_auth():
 	pass
 
-
-@app.route('/postGroup', methods=['POST'])
-def post_group():
-	pass
+Mixed Reality & the Metaverse (AR/VR - Augmented & Virtual Reality)
+Quantum Computing
+Nanotechnology or Nanobiotechnology
+Bioprinting
+Brain-machine interface (e.g. Neuralink)
+IoT & Cloud Computing
+Autonomous Vehicles
+Commercial UAV (advanced autonomous drones)
+Digital Fabrication (laser cutting, CNC machining, 3D printing, etc.)
+Blockchain
 '''
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
