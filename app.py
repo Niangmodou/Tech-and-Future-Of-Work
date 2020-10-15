87f6378b55faf83dc7bcf127838578b1b99fac8b
Modou Niang <modouniang@modous-mbp.attlocal.net>
2020-10-15 00:04:04 -0500

    trying x1000

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 os.environ['DATABASE_URL'] = "postgresql://localhost/tech-future-of-work"
 app.config.from_object(os.environ['APP_SETTINGS'])
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/tech-future-of-work"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/tech-future-of-work"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
